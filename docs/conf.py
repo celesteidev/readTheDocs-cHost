71439872abaf3195bef19dfe1d1e6f04580a4813
celesteidev <mikiepiek2013@gmail.com>
2025-10-21 17:25:12 +0100

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+project = 'cHost Help Docs'
+copyright = '2025 HCL Celestei cHost'
+author = 'support@host.celestei.com'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
